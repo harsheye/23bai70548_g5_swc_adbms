6c2b2f3e31c65f8c421e83fe2d323dd4d7e45ef5
Harsh <pc.harsh.mail@gmail.com>
2026-06-24 10:46:44 +0530

    bakaa

---
+++ Session 4/Session 4_HW.sql
@@ -0,0 +1,10 @@
+SELECT 
+    ROUND(AVG(CASE WHEN s.order_date = d.customer_pref_delivery_date THEN 1 ELSE 0 END) * 100, 2) AS immediate_percentage
+FROM (
+    SELECT customer_id, MIN(order_date) AS order_date
+    FROM Delivery 
+    GROUP BY customer_id
+) AS s
+JOIN Delivery d 
+    ON s.customer_id = d.customer_id
+   AND s.order_date = d.order_date;
